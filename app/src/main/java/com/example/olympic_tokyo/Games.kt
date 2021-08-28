e2261ecfaf5ffa71429ce2b8c8a0c9e00f2cb7a5
huda997 <Hudamed97@gmail.com>
2021-08-28 05:06:11 +0300

    add Games class

---
+++ app/src/main/java/com/example/olympic_tokyo/Games.kt
@@ -0,0 +1,13 @@
+package com.example.olympic_tokyo
+
+class Games(
+    val team: String,
+    val rank: Int,
+    val goldMedal:String,
+    val silverMedal:String,
+    val bronzeMedal:String,
+    val totalMedalsOfTeam:Int,
+    val rankByTotal:Int,
+    val NOCCode:String)
+ {
+}
